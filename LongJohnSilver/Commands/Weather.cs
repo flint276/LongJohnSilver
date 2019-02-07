712e7a96b2829d2461f63cbbb76955e54920383c
Philip Dando <flint276@hotmail.com>
2019-02-07 20:12:15 +0000

    Added weather function

---
+++ LongJohnSilver/Commands/Weather.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework.Constraints;
+
+namespace LongJohnSilver.Commands
+{
+    public class Weather : ModuleBase<SocketCommandContext>
+    {
+        [Command("weather")]
+        public async Task WeatherReportAsync([Remainder] string input = "")
+        {
+            using (var w = new WebClient())
+            {
+                var jsonString = string.Empty;
+                // ReSharper disable once StringLiteralTypo
+                var url = $"http://api.openweathermap.org/data/2.5/weather?q={input}&APPID=d92f7020874af5fb9b945e78ebb25106";
+
+                jsonString = w.DownloadString(url);
+
+
+                dynamic array = JObject.Parse(jsonString);
+
+                string townname = array.name;
+                string country = array.sys.country;
+                string description = array.weather[0].description;
+                string icon = array.weather[0].icon;
+                string weatherSymbol = String.Empty;
+
+                switch (icon)
+                {
+                    case "01d":
+                        weatherSymbol = ":sunny:";
+                        break;
+                    case "01n":
+                        weatherSymbol = ":first_quarter_moon_with_face:";
+                        break;
+                    case "02d":
+                        weatherSymbol = ":white_sun_cloud:";
+                        break;
+                    case "02n":
+                        weatherSymbol = ":cloud:";
+                        break;
+                    case "03d":
+                        weatherSymbol = ":cloud:";
+                        break;
+                    case "03n":
+                        weatherSymbol = ":cloud:";
+                        break;
+                    case "04d":
+                        weatherSymbol = ":cloud:";
+                        break;
+                    case "04n":
+                        weatherSymbol = ":cloud:";
+                        break;
+                    case "09d":
+                        weatherSymbol = ":cloud_rain:";
+                        break;
+                    case "09n":
+                        weatherSymbol = ":cloud_rain:";
+                        break;
+                    case "10d":
+                        weatherSymbol = ":cloud_rain:";
+                        break;
+                    case "10n":
+                        weatherSymbol = ":cloud_rain:";
+                        break;
+                    case "11d":
+                        weatherSymbol = ":thunder_cloud_rain:";
+                        break;
+                    case "11n":
+                        weatherSymbol = ":thunder_cloud_rain:";
+                        break;
+                    case "13d":
+                    case "13n":
+                        weatherSymbol = ":cloud_snow:";
+                        break;
+                    case "50d":
+                    case "50n":
+                        weatherSymbol = ":foggy:";
+                        break;
+                    default:
+                        weatherSymbol = ":question:";
+                        break;
+                }
+                
+
+                float temp = array.main.temp;
+                var tempC = Math.Round(temp - 273.15, 1);
+                var tempF = Math.Round(temp * 9 / 5 - 459.67, 1);
+
+                var embed = new EmbedBuilder();
+                embed.WithAuthor($"{townname}, {country}");
+                embed.WithColor(40, 200, 150);
+                embed.WithDescription($"{weatherSymbol}  *{description}*\n:thermometer:{tempC}c   :thermometer:{tempF}f");
+
+                await Context.Channel.SendMessageAsync("", false, embed.Build());
+
+                /*
+                await Context.Channel.SendMessageAsync($"{townname}, {country}");
+                await Context.Channel.SendMessageAsync($"{description}");
+                await Context.Channel.SendMessageAsync($"{icon}");
+                await Context.Channel.SendMessageAsync($"Temperature: C:{tempc} F:{tempf}");
+                */
+
+            }
+        }
+    }
+}
